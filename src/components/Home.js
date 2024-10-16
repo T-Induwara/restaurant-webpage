40664810a8774768af9a082b7b4da5a6adef0bad
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-10-16 12:15:51 +0530

    Added background image and text fields

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import BannerBackground from '../assets/home-banner-background.png';
 import BannerImage from '../assets/home-banner-image.png';
-import {FiaArrowRight} from 'react-icons/fi';
+import {FiArrowRight} from 'react-icons/fi';
 
 function Home(){
     return(
@@ -10,7 +10,18 @@ function Home(){
             <Navbar/>
             <div className="home-banner-container">
                 <div className="home-bannerImage-container">
-                    <img src="" alt=""></img>
+                    <img src={BannerBackground} alt="Banner background"></img>
+                </div>
+                <div className="home-text-section">
+                    <h1 className="primary-heading">
+                        Your Favourite Food Delivered Hot & Fresh
+                    </h1>
+                    <p className="primary-text">
+                        Our professional chefs do all the prep wor. So you can cook fresh food.
+                    </p>
+                    <button className="secondary-button">
+                        Order now <FiArrowRight/>
+                    </button>
                 </div>
             </div>
         </div>
